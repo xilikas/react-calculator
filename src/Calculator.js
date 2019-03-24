986eac7b86d40342906d8f5ae3814fa0daf5c61e
Fil Luzadas <flg.l@outlook.com>
2019-03-24 10:26:12 -0400

    Styled calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -75,22 +75,24 @@ export default class Calculator extends Component {
           {result !== 0 && result}
         </h3>
         <CalcGrid>
-          <button onClick={() => this.input('c')} type="button">C</button>
           <button onClick={() => this.input(1)} type="button">1</button>
           <button onClick={() => this.input(2)} type="button">2</button>
           <button onClick={() => this.input(3)} type="button">3</button>
+          <button onClick={() => this.input('+')} type="button">+</button>
           <button onClick={() => this.input(4)} type="button">4</button>
           <button onClick={() => this.input(5)} type="button">5</button>
           <button onClick={() => this.input(6)} type="button">6</button>
+          <button onClick={() => this.input('-')} type="button">-</button>
           <button onClick={() => this.input(7)} type="button">7</button>
           <button onClick={() => this.input(8)} type="button">8</button>
           <button onClick={() => this.input(9)} type="button">9</button>
-          <button onClick={() => this.input(0)} type="button">0</button>
-          <button onClick={() => this.input('+')} type="button">+</button>
           <button onClick={() => this.input('×')} type="button">×</button>
-          <button onClick={() => this.input('-')} type="button">-</button>
+          <div />
+          <button onClick={() => this.input(0)} type="button">0</button>
+          <div />
           <button onClick={() => this.input('÷')} type="button">÷</button>
-          <button onClick={() => this.input('=')} type="button"> =</button>
+          <button className="eq" onClick={() => this.input('=')} type="button"> =</button>
+          <button className="c" onClick={() => this.input('c')} type="button">C</button>
         </CalcGrid>
       </div>
     );
@@ -99,7 +101,15 @@ export default class Calculator extends Component {
 
 const CalcGrid = styled.div`
   display: grid;
-  padding: 2rem;
-  grid-template-columns: repeat (3, 1fr);
-  grid-row-gap: 1rem;
+  margin: auto;
+  max-width: 500px;
+  justify-content: center;
+  grid-template-columns: 50px 50px 50px 50px;
+  grid-gap: 10px;
+  > .c {
+    grid-area: 1 / 1 / -1 / -1;
+  }
+  > .eq {
+    grid-area: 6 / 1 / 6 / -1;
+  }
 `;
